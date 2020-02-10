4cde5f3d2df16d528701f6056f29e2c9465ba0b6
manchandajayant <maanjayant@gmail.com>
2020-02-10 16:26:36 +0100

    intial changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { render } from "@testing-library/react";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header>
+        <nav id="nav">
+          <p>Make a Cliffhanger</p>
+        </nav>
       </header>
     </div>
   );
