19cd997acd8b37c574753c2a6be8419fd034e2f7
manchandajayant <maanjayant@gmail.com>
2020-02-11 16:00:16 +0100

    new-stuff

--- src/App.js
+++ src/App.js
@@ -10,6 +10,12 @@ function App() {
           <p>Make a Cliffhanger</p>
         </nav>
       </header>
+      <div>
+        <img id="image" src="nature.png"></img>
+      </div>
+      <div>
+        <input id="comment"></input>
+      </div>
     </div>
   );
 }
